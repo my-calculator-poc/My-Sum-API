f8466cabd16849bfc53a788a4d62a125bc01a92c
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-06-18 10:14:22 +0200

    Adding Swagger Spec Adding Contracts Adding Minimum Spring Boot project files Adding CI Support (Travis)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # My-Sum-API
 My Sum API
+
+## Desing
+
+The API design is based on Swagger.
+https://editor.swagger.io/
+
+
---
+++ src/test/java/org/jab/microservices/scc/verifier/SumBase.java
@@ -0,0 +1,28 @@
+package org.jab.microservices.scc.verifier;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.jab.microservices.MainApplication;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * Base class for the generated Spring Cloud Contract verification tests.
+ * <p>
+ * This test validates the contract of the API, including response codes, headers, content type, etc.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MainApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SumBase {
+
+    @Autowired
+    WebApplicationContext applicationContext;
+
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.webAppContextSetup(applicationContext);
+    }
+}
